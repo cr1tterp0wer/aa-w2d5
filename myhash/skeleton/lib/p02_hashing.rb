eb26b8f48a02c03c3fab5ee46a812dfd3df055a2
App Academy Student <student@appacademy.io>
2017-08-11 14:55:57 -0700

    hash_set solved

--- myhash/skeleton/lib/p02_hashing.rb
+++ myhash/skeleton/lib/p02_hashing.rb
@@ -1,14 +1,44 @@
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index do |el, i|
+      prime = get_prime(i+1)
+      sum += ((el.hash + prime)).hash
+    end
+    sum
   end
+
+  def is_prime?(n)
+    return false if n<=1
+    (2...n).none? {|i| n % i ==0}
+  end
+
+  def get_prime(n)
+    i = 1
+    primes= []
+    until primes.length == n
+      primes  << i if is_prime?(i)
+      i += 1
+    end
+    primes.last
+  end
+
 end
 
 class String
   def hash
+    self.split('').map(&:ord).hash
+  end
+end
+
+class Symbol
+  def hash
+    self.to_s.hash
   end
 end
 
@@ -16,6 +46,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+
+    key_val_pairs = []
+    self.to_a.sort.each do |tupe|
+      key_val_pairs << tupe.hash
+    end
+
+    key_val_pairs.sort.hash
   end
 end
--- myhash/skeleton/lib/p03_hash_set.rb
+++ myhash/skeleton/lib/p03_hash_set.rb
@@ -9,18 +9,33 @@ class HashSet
   end
 
   def insert(key)
+
+    self[key] << key unless include?(key)
+    @count += 1
+    if @count >= num_buckets
+      @count = 0
+      resize!
+    end
+
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
+  end
+
+  def []=(num, val)
+    @store[num.hash % num_buckets] = val
   end
 
   def num_buckets
@@ -28,5 +43,14 @@ class HashSet
   end
 
   def resize!
+    values = @store.flatten
+    old_length = num_buckets
+    new_length = old_length * 2
+
+    @store = Array.new(new_length) {Array.new}
+    values.each do |el|
+      insert(el)
+    end
+
   end
 end
